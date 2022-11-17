41a85e6d514cf9d37ea1ca641d60f8649bf82f45
Evan Dwight <effdem3@gmail.com>
2022-11-17 16:11:34 -0600

    Fix video on ios

--- components/MediaElement/VideoHandlerHack.tsx
+++ components/MediaElement/VideoHandlerHack.tsx
@@ -1,19 +1,30 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 
-export default function VideoHandler({ videoUrl, audioUrl, onLoadingComplete }) {
+export default function VideoHandlerHack({ videoUrl, audioUrl, onLoadingComplete }) {
     const audioRef = useRef<HTMLAudioElement>(null);
     const videoRef = useRef<HTMLVideoElement>(null);
+    const [userStarted, setUserStarted] = useState(false);
     return <div>
-        <audio ref={audioRef} src={audioUrl}/>
+        {!userStarted && <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            <button onClick={() => {
+                if (audioRef.current && videoRef.current) {
+                    audioRef.current.play();
+                    videoRef.current.play();
+                    audioRef.current?.fastSeek(videoRef.current.currentTime);
+                    setUserStarted(true);
+                }
+            }}>Start video</button>
+        </div>}
+        <audio ref={audioRef} loop src={audioUrl} />
         <video ref={videoRef} className="max-h-full max-w-full"
-        controls muted loop preload="auto" playsInline draggable={false}
-        onLoadStart={onLoadingComplete}
-        onSeeked={(event) => videoRef.current && audioRef.current?.fastSeek(videoRef.current.currentTime)}
-        onPause={(event) => audioRef.current?.pause()}
-        onPlay={(event) => audioRef.current?.play()}>
-        <source data-src={videoUrl} src={videoUrl} type="video/mp4" />
-    </video>
-        
-    </div>
+            muted loop preload="auto" playsInline draggable={false}
+            controls={userStarted}
+            onLoadStart={onLoadingComplete}
+            onSeeked={(event) => videoRef.current && audioRef.current?.fastSeek(videoRef.current.currentTime)}
+            onPause={(event) => audioRef.current?.pause()}
+            onPlay={(event) => audioRef.current?.play()}>
+            <source data-src={videoUrl} src={videoUrl} type="video/mp4" />
+        </video>
+    </div >
 }
