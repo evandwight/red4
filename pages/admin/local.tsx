60cfcecdfe86ce99ecb28a1de10b1a086169b798
Evan Dwight <effdem3@gmail.com>
2022-11-18 16:29:50 -0600

    List profiles

--- pages/admin/local.tsx
+++ pages/admin/local.tsx
@@ -1,35 +1,56 @@
+import { profile } from "@prisma/client";
 import { CommentInfo } from "components/Comment/Comments";
 import { PostSmall, UserText } from "components/Post";
 import { Tags } from "components/Thing/Tags";
 import { assertAdmin } from "lib/api/utils";
-import { CommentType, PostType } from "lib/commonTypes";
+import { CommentType, PostType, ProfileType } from "lib/commonTypes";
 import getTags from "lib/getTags";
 import prisma from "lib/prisma";
 import { NextPageContext } from "next";
+import { ApiError } from "next/dist/server/api-utils";
 
 export async function getServerSideProps(context: NextPageContext) {
-    if (!context.req) {
-        throw new Error("Request undefined");
-    }
-    await assertAdmin(context.req);
-    const clean = (x) => JSON.parse(JSON.stringify(x));
-    const posts = clean(await prisma.post.findMany({ where: { is_local: true } }));
-    const comments = clean(await prisma.comment.findMany({ where: { is_local: true } }));
-    const tagMap = getTags(posts.concat(comments).map(thing => thing.id));
-    const addTags = (things) => things.map(thing => ({...things, tags: tagMap[thing.id] || []}));
-    return {
-        props: { posts: addTags(posts), comments: addTags(comments) },
+    try {
+        if (!context.req) {
+            throw new Error("Request undefined");
+        }
+        await assertAdmin(context.req);
+        const clean = (x) => JSON.parse(JSON.stringify(x));
+        const posts = clean(await prisma.post.findMany({ where: { is_local: true } }));
+        const comments = clean(await prisma.comment.findMany({ where: { is_local: true } }));
+        const tagMap = getTags(posts.concat(comments).map(thing => thing.id));
+        const addTags = (things) => things.map(thing => ({ ...things, tags: tagMap[thing.id] || [] }));
+        const profiles = await prisma.profile.findMany();
+        return {
+            props: { posts: addTags(posts), comments: addTags(comments), profiles },
+        }
+    } catch (error) {
+        if (error instanceof ApiError) {
+            return { props: { error: { statusCode: error.statusCode, message: error.message } } };
+        } else {
+            return { props: { error: { statusCode: 500, message: "Internal server error" } } }
+        }
+
     }
 }
 
+type PropsType = { posts: PostType[], comments: CommentType[], profiles: profile[] }
+    | { error: { statusCode: number, message: string } };
+const Local = (props: PropsType) => {
+    if ("error" in props) {
+        const { statusCode, message } = props.error;
+        return <div>
+            Error {statusCode} - {message}
+        </div>
+    }
 
-const Local = ({ posts, comments }: { posts: PostType[], comments: CommentType[] }) => {
+    const { posts, comments, profiles } = props;
     return <div>
-        <div>Local posts:</div>
+        <h2>Local posts:</h2>
         <div>
             {posts.map(post => <PostSmall key={post.id} {... { post, initialVotes: {}, profile: {} }} />)}
         </div>
-        <div>Local comments:</div>
+        <h2>Local comments:</h2>
         <div>
             {comments.map(comment => <div key={comment.id}>
                 <CommentInfo {...{ comment, expand: true, toggleExpand: () => { } }} />
@@ -37,6 +58,12 @@ const Local = ({ posts, comments }: { posts: PostType[], comments: CommentType[]
                     <Tags thing={comment} />
                     <div><UserText text={comment.text} /></div>
                 </div>
+            </div>)}
+        </div>
+        <h2>Profiles:</h2>
+        <div>
+            {profiles.map(profile => <div key={profile.id}>
+                {profile.user_name} {JSON.stringify(profile.tag_filter)}
             </div>)}
         </div>
     </div>
