17d44862c9d86dd0547001c6857dfbda48e51294
Evan Dwight <effdem3@gmail.com>
2022-11-18 00:54:24 -0600

    Actually run the script

--- scripts/runJs.sh
+++ scripts/runJs.sh
@@ -1,3 +1,4 @@
 export NODE_PATH=$1/build-scripts:$1
 export $(grep -v '^#' $1/.env | xargs)
-node $1/build-scripts/scripts/reddit/$2 & echo "success"
+node $1/build-scripts/scripts/reddit/$2 
+echo "success"
