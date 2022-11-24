0d9890f466b4d5a923a98a4f0c6435b6d843756b
Evan Dwight <effdem3@gmail.com>
2022-11-24 11:47:54 -0600

    Fix userText

--- lib/__tests__/userText.test.ts
+++ lib/__tests__/userText.test.ts
@@ -14,13 +14,27 @@ describe('extractLinks', () => {
         expect(links).toEqual(['1234', '2234']);
     })
     it('handles multiple lines', async () => {
-        const rawText = `[asdf](1234) [asdf2](2234)
-[line2](3234) sdfwr [line22](4234)
- [line3](5234)`;
+        const rawText = "[asdf](1234) [asdf2](2234)\n[line2](3234) sdfwr [line22](4234)\n[line3](5234)";
         const {text, links} = extractLinks(rawText);
-        expect(text).toEqual(`asdf[1] asdf2[2]
-line2[3] sdfwr line22[4]
- line3[5]`);
+        expect(text).toEqual(`asdf[1] asdf2[2]\nline2[3] sdfwr line22[4]\nline3[5]`);
         expect(links).toEqual(['1234', '2234', '3234', '4234', '5234']);
+    })
+    it('handles escapes', async () => {
+        const rawText = '[[asdf\\]]((1234\\))';
+        const {text, links} = extractLinks(rawText);
+        expect(text).toEqual('[asdf][1]');
+        expect(links).toEqual(['(1234)']);
+    })
+    it('handles escaped links', async () => {
+        const rawText = '\\[asdf](1234)';
+        const {text, links} = extractLinks(rawText);
+        expect(text).toEqual('[asdf](1234)');
+        expect(links).toEqual([]);
+    })
+    it('handles brackets inside', async () => {
+        const rawText = "---\n\n[^(source code)](https://amirror.link/source) ^| [^(run your own mirror bot? let's integrate)](https://amirror.link/lets-talk)";
+        const {text, links} = extractLinks(rawText);
+        expect(text).toEqual("---\n\n^(source code)[1] ^| ^(run your own mirror bot? let's integrate)[2]");
+        expect(links).toEqual(['https://amirror.link/source', 'https://amirror.link/lets-talk']);
     })
 })
--- lib/userText.ts
+++ lib/userText.ts
@@ -1,12 +1,18 @@
+export function unescapeBackslash(text) {
+    return text.replace(/\\(.)/g, "$1");
+}
+
 export function extractLinks(text: string) {
-    const links: string[] = [];
-    const linkRegex = /(^|[^\\])\[((?:[^\n\]]|\\\])+)\]\(((?:[^\n\]]|\\\))+)\)/g;
-    text = text.replace(linkRegex, (match, p1, p2, p3) => {
-        console.log({p1,p2,p3});
-        links.push(p3);
-        return `${p1}${p2}[${links.length}]`
-    });
-    console.log(text);
+    let links: string[] = [];
+    // \ = \x5C
+    text = text.replace(/(^|[^\x5c])\[((?:[^\]\x5C]|\x5C.)+)\]\(((?:[^\)\x5C]|\x5C.)+)\)/mg,
+        (match, p1, p2, p3) => {
+            links.push(p3);
+            return `${p1}${p2}[${links.length}]`
+        });
+
+    text = unescapeBackslash(text);
+    links = links.map(unescapeBackslash);
     return {
         text,
         links,
