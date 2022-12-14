e222d5b781d089eebc0e669e094a54f4fda4382f
Evan Dwight <effdem3@gmail.com>
2022-12-14 08:36:38 -0600

    Change default profile

--- pages/api/profile.ts
+++ pages/api/profile.ts
@@ -1,7 +1,13 @@
 import { fancyApi } from 'lib/api/fancyApi';
 import { API_GET_PROFILE } from 'lib/api/paths';
 
-export const defaultProfile: any = { tag_filter: [] };
+export const defaultProfile: any = {
+    tag_filter: {
+        reddit_deleted: false,
+        reddit_removed: false,
+        reddit_locked: false,
+    }
+};
 
 const handler = fancyApi(API_GET_PROFILE, { maybeWithUserIdAndProfile: true, }, async (req, res, props) => {
     res.status(200).json({ profile: props.profile || defaultProfile })
